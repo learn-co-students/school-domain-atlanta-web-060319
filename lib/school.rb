3629e9dcf08498abde31a4ebbe6da124e2bb6bb0
Jau-Tzuoo Chen <robertfromsms@gmail.com>
2019-06-05 18:23:30 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
-# code here!
+class School
+	attr_accessor :name
+	attr_reader :roster
+
+	def initialize(name)
+		@name = name
+		@roster = {}
+	end
+
+	def add_student(name, grade)
+		if @roster[grade] == nil
+			@roster[grade] = [].push(name)
+		else
+			@roster[grade].push(name)
+		end
+	end
+
+	def grade(grade)
+		self.roster[grade]
+	end
+
+	def sort
+		self.roster.each {|grade, students|
+			@roster[grade] = students.sort
+		}
+	end
+end
+
